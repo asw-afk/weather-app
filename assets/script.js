8ff0d979071971f882036762d8d6817d6807b503
asw <annasilverwolff@gmail.com>
2024-05-23 04:04:20 -0400

    flex bug introduced

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+const APIKey = "41779050d33997f1e038c319cda10442";
+
+let city; 
+
+const queryURL = `api.openweathermap.org/data/2.5/weather?q={city name}&appid={41779050d33997f1e038c319cda10442}`;
