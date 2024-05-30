06fbed914fda16067e1858a3f87f4a4a0833a94c
asw <annasilverwolff@gmail.com>
2024-05-29 20:36:02 -0400

    close to mvp

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,38 @@
 const APIKey = "41779050d33997f1e038c319cda10442";
 
-let city; 
-
-// const queryURL = `api.openweathermap.org/data/2.5/weather?q={city name}&appid={41779050d33997f1e038c319cda10442}`;
-
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?=metric&q=";
 
+//Variable from HTML, needed to to grab search inputs/button
 const searchBox = document.querySelector(".search input")
 const searchBtn = document.querySelector(".search button")
+const weatherIcon = document.querySelector(".weather-icon")
 
+//Grabs the data from the weather API and Renders it
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${APIKey}`);
     var data = await response.json();
 
     console.log(data);
 
+    //query selectors to grab fetch data and change the HTML
     document.querySelector(".city").innerHTML = data.name
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
-
+    //Else if to change icons based on API data
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "assets/images/clouds.png";
+    } 
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "assets/images/rain.png";
+    } 
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "assets/images/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "assets/images/mist.png";
+    }
 
 }
 
@@ -29,32 +41,9 @@ searchBtn.addEventListener("click", ()=>{
 
 })
 
-
-// Dom Element references 
-const searchForum = document.querySelector('#search-form');
-const searchInput = document.querySelector('#search-input');
-const todayContainer = document.querySelector('#today');
-
-function getWeather(city,weather){
-   
-    const tempC = weather.main.temp;
-    const humidity = wewather.main.humidity; 
-
-    const tempEl = document.createElement('p');
-    const humidityEl = document.createElement('p');
-
-
-    tempEl.textContent = `Temp: ${tempC}C`;
-    humidityEl.textContent = `Humidity: ${humidity}`;
-
-    console.log(test);
-    
-todayContainer.innerHTML = '';
-}
-
-
-searchButton.addEventListener('click', function (event) {
-   
-    getWeather;
-    console.log("dfs");
-});
+// document.querySelector('#search').addEventListener('keypress', function (checkWeather) {
+//     if (e.key === 'Enter') {
+//       // code for enter
+      
+//     }
+// });
