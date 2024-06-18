07b18b0332b30a38fee4d7dc2b75bd1c2a85f49e
asw <annasilverwolff@gmail.com>
2024-06-18 00:50:38 -0400

    localstorage functionality working

--- assets/script.js
+++ assets/script.js
@@ -7,18 +7,24 @@ const searchBox = document.querySelector(".search input")
 const searchBtn = document.querySelector(".search button")
 const weatherIcon = document.querySelector(".weather-icon")
 
+const citiesSearchedSpan = document.querySelector(".cities-searched")
+
+
+
 //Grabs the data from the weather API and Renders it
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${APIKey}`);
     var data = await response.json();
 
     console.log(data);
+ 
 
     //query selectors to grab fetch data and change the HTML
     document.querySelector(".city").innerHTML = data.name
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
 
     //Else if to change icons based on API data
     if(data.weather[0].main == "Clouds"){
@@ -34,12 +40,47 @@ async function checkWeather(city){
         weatherIcon.src = "assets/images/mist.png";
     }
 
+    
 }
 
+
+
+// console.log(data, 'data set');
+
+async function citySaver(city) {
+    const response = await fetch(apiUrl + city + `&appid=${APIKey}`);
+    var data = await response.json();
+   
+    let currentData = {
+        name: data.name,
+        data: data.main.date,
+        temp: data.main.temp,
+        humidity: data.main.temp,
+        wind: data.wind.speed,
+        icon: data.weather[0].icon
+    }
+    console.log(currentData)
+    localStorage.setItem('currentData', JSON.stringify(currentData));
+}
+
+
+function renderCitySearch() {
+    
+}
+
+
+
 searchBtn.addEventListener("click", ()=>{
+   
     checkWeather(searchBox.value);
 
+   citySaver(searchBox.value);
+    
+
+    
 })
+
+
 
 // document.querySelector('#search').addEventListener('keypress', function (checkWeather) {
 //     if (e.key === 'Enter') {
